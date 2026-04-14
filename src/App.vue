694e95d309657c440663728a4b620776c1d461db
carolxiaoching <carol.xiao.ching@gmail.com>
2026-04-14 08:03:11 +0800

    refactor: 💡 完成 App.vue 遷移與 TS 重構優化

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-  import { RouterView } from 'vue-router';
+  import { RouterView, useRoute } from 'vue-router';
+  import { storeToRefs } from 'pinia';
+  import { computed } from 'vue';
+  import Loading from 'vue-loading-overlay';
+  import 'vue-loading-overlay/dist/css/index.css';
+  import { loadingStore } from '@/stores';
+  import Toast from '@/components/ToastComponent.vue';
+
+  const loadingRef = loadingStore();
+  const { isLoading } = storeToRefs(loadingRef);
+
+  const route = useRoute();
+  const routerKey = computed(() => (route.name === 'user' ? route.fullPath : route.path));
 </script>
 
 <template>
-  <RouterView />
+  <Loading v-model:active="isLoading" :can-cancel="false" :is-full-page="true" :z-index="1060" />
+  <Toast />
+  <RouterView :key="routerKey" />
 </template>
-
-<style lang="scss">
-  @import '@/assets/scss/all.scss';
-</style>
